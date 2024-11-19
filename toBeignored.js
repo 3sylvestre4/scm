c6b21e32ad41a842389ee3af25dcafe5b6236683
3sylvestre4 <ingrolando.pierre@gmail.com>
2024-11-19 12:32:40 -0500

    updated gitignore

--- toBeignored.js
+++
@@ -1,2 +0,0 @@
-this file is supposed to be ignored. but i forgot to add it to .gitignore, i already staged it
-and commited it. in order to remove it from the tracked files, i need to rtoBeignored.js
